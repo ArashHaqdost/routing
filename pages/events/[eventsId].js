f582d21d900c10d6ec59f59283a23cd9a11b484a
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-09-20 22:04:01 +0430

    added evetn

--- pages/events/[eventsId].js
+++ pages/events/[eventsId].js
@@ -1,8 +1,35 @@
-import React from 'react'
+import {useRouter} from 'next/router'
+
+import {getEventById} from '../../dummy-data'
+import { Fragment } from 'react';
+
+import EventContent from '../../components/event-detail/event-content';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventSummary from '../../components/event-detail/event-summary';
+import LogisticsItem from '../../components/event-detail/logistics-item';
 
 function EvetnsDetails() {
+  const  router = useRouter();
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+  console.log(event)
+  if(!event){
+    return(<h3>no Info</h3>)
+  }
   return (
-    <div>EvetnsDetails page</div>
+    <Fragment>
+    <EventSummary title={event.title} />
+    <EventLogistics 
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+    />
+    <EventContent>
+        <p>{event.description}</p>
+    </EventContent>
+</Fragment>
   )
 }
 
