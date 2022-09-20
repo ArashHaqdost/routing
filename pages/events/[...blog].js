aeb2a20d35a6ba8113b623f1761336c1ac8eb6d7
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-09-20 22:02:30 +0430

    added daynamic rout

--- pages/events/[...blog].js
+++ pages/events/[...blog].js
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import {useRouter} from 'next/router'
+import {getFilteredEvents} from '../../dummy-data'
+import EvetnsList from '../../components/events/Events-list';
+import Button from '../../components/ui/Button';
 
 function EvetnsSpecific() {
+  const router = useRouter();
+  const filterData= router.query.blog;
+
+    if(!filterData){
+      return <p className='center'>Loading...</p>
+    }
+
+    const filterYear = filterData[0];
+    const  filterMoth = filterData[1];
+
+    const numYear = +filterYear;
+    const numMonth = +filterMoth;
+
+    if (
+      isNaN(numYear) ||
+      isNaN(numMonth) ||
+      numYear > 2030 ||
+      numYear < 2021 ||
+      numMonth > 12 ||
+      numMonth < 1
+    ){
+      return(
+         <Fragment>
+           <p>Not Valid Endter Valid filter </p>
+           <div className='center'>
+            <Button link='/events'>Show All events</Button>
+           </div>
+        
+         </Fragment>
+)    }
+
+    const filteredEvents = getFilteredEvents({
+      year:numYear,
+      month: numMonth,
+    })
+
+    if(!filteredEvents || filteredEvents.length===0){
+      return (
+        <Fragment>
+          <p>NO evetn found for the  chosen filted</p>
+          <div className='center'>
+           <Button link='/events'>Show All events</Button>
+          </div>
+       
+        </Fragment>
+)  
+    }
   return (
-    <div>EvetnsSpecific page</div>
+    <div><EvetnsList items={filteredEvents} /></div>
   )
 }
 
