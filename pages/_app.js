7d52c897871425cafe100781ef1dd47363fd482e
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-09-20 22:02:00 +0430

    added featurr of app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import Laytout from '../components/layout/Laytout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Laytout>
+      <Component {...pageProps} />
+    </Laytout>
+  )
 }
 
 export default MyApp
